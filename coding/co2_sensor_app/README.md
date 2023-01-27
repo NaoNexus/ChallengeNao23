4fa066150474e15cd8b6a7855f160fbe70b91e7c
Bernaaaa <francescobernardi04@gmail.com>
2023-01-27 14:15:49 +0100

    Added app

---
+++ coding/co2_sensor_app/README.md
@@ -0,0 +1,3 @@
+# co2_sensor_app
+
+A new Flutter project.
---
+++ coding/co2_sensor_app/android/app/src/main/kotlin/com/example/co2_sensor_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.co2_sensor_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
