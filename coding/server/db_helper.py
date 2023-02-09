4758dc24131d79649a216f2a47a0f6e1c0e213a7
Riccardo Antonelli <rickyanto05@gmail.com>
2023-02-09 22:55:53 +0100

    Changed UI in reports page and updated DB with new fields

--- coding/server/db_helper.py
+++ coding/server/db_helper.py
@@ -17,9 +17,9 @@ class DB:
                         temperature NUMERIC(5, 2) NOT NULL,
                         co2 NUMERIC(6),
                         humidity NUMERIC(3),
-                        nPeople NUMERIC(4),
-                        interiorLight NUMERIC(4),
-                        exteriorLight NUMERIC(4));''')
+                        nPeople NUMERIC(4) DEFAULT 0,
+                        internalLight NUMERIC(4) DEFAULT 0,
+                        externalLight NUMERIC(4) DEFAULT 0);''')
 
                 print('DB initialized:', cur.statusmessage)
 
@@ -31,16 +31,16 @@ class DB:
 
                 if (report.get('id', '') == ''):
                     cur.execute('''
-                        INSERT INTO Reports(date, temperature, co2, humidity, nPeople, interiorLight, exteriorLight)
+                        INSERT INTO Reports(date, temperature, co2, humidity, "nPeople", "internalLight", "externalLight")
                         VALUES (%s, %s, %s, %s, %s, %s, %s);''',
-                                (report['date'], report['temperature'], report['co2'], report['humidity'], report.get('nPeople', 0), report.get('interiorLight', 0), report.get('exteriorLight', 0),))
+                                (report['date'], report['temperature'], report['co2'], report['humidity'], report.get('nPeople', 0), report.get('internalLight', 0), report.get('externalLight', 0),))
                 else:
                     cur.execute('''
                         UPDATE Reports
-                        SET date = %s, temperature = %s, co2 = %s, humidity = %s, nPeople = %s, interiorLight = %s, exteriorLight = %s
+                        SET date = %s, temperature = %s, co2 = %s, humidity = %s, "nPeople" = %s, "internalLight" = %s, "externalLight" = %s
                         WHERE id = %s
                         ORDER BY date;''',
-                                (report['date'], report['temperature'], report['co2'], report['humidity'], report['nPeople'], report['interiorLight'], report['exteriorLight'], report['id']))
+                                (report['date'], report['temperature'], report['co2'], report['humidity'], report['nPeople'], report['internalLight'], report['externalLight'], report['id']))
 
                 return cur.statusmessage
 
@@ -56,7 +56,7 @@ class DB:
                 if (cur.rowcount == 0):
                     return {}
                 for tupla in cur:
-                    return {'id': tupla[0], 'date': tupla[1], 'temperature': tupla[2], 'co2': tupla[3], 'humidity': tupla[4]}
+                    return {'id': tupla[0], 'date': tupla[1], 'temperature': tupla[2], 'co2': tupla[3], 'humidity': tupla[4], 'nPeople': tupla[5], 'internalLight': tupla[6], 'externalLight': tupla[7]}
 
     def get_reports(self):
         with self.connection:
@@ -68,6 +68,6 @@ class DB:
 
                 for tupla in cur:
                     data.append(
-                        {'id': tupla[0], 'date': tupla[1], 'temperature': tupla[2], 'co2': tupla[3], 'humidity': tupla[4]})
+                        {'id': tupla[0], 'date': tupla[1], 'temperature': tupla[2], 'co2': tupla[3], 'humidity': tupla[4], 'nPeople': tupla[5], 'internalLight': tupla[6], 'externalLight': tupla[7]})
 
                 return data
