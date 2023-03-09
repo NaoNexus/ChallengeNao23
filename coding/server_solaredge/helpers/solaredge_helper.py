dcd3be19803f0a834e28a006bb7218d6c0417a26
Riccardo Antonelli <rickyanto05@gmail.com>
2023-03-09 20:25:07 +0100

    Updated solaredge server

--- coding/server_solaredge/helpers/solaredge_helper.py
+++ coding/server_solaredge/helpers/solaredge_helper.py
@@ -9,39 +9,41 @@ class SolarEdge():
         self.username = config.solaredge_username
         self.password = config.solaredge_password
 
+        self.initialize_selenium()
+
         self.login()
 
-    def input(input, value):
+    def input(self, input, value):
         {
-            'new_project': function,
-            'info_section': function,
-            'modelling_section': function,
-            'positioning_section': function,
-            'storage_section': function,
-            'electrical_section': function,
-            'financial_section': function,
-            'summary_section': function,
-            'project_type': function,
-            'project_name': function,
-            'country': function,
-            'street': function,
-            'city': function,
-            'zip': function,
-            'consumption': function,
-            'consumption_period': function,
-            'electrical_grid': function,
-            'power_factor': function,
-            'name': function,
-            'surname': function,
-            'company': function,
-            'notes': function,
+            'new_project': self.function,
+            'info_section': self.function,
+            'modelling_section': self.function,
+            'positioning_section': self.function,
+            'storage_section': self.function,
+            'electrical_section': self.function,
+            'financial_section': self.function,
+            'summary_section': self.function,
+            'project_type': self.function,
+            'project_name': self.function,
+            'country': self.function,
+            'street': self.function,
+            'city': self.function,
+            'zip': self.function,
+            'consumption': self.function,
+            'consumption_period': self.function,
+            'electrical_grid': self.function,
+            'power_factor': self.function,
+            'name': self.function,
+            'surname': self.function,
+            'company': self.function,
+            'notes': self.function,
         }[input](value)
 
-        def function(self, value):
-            pass
+    def function(self, value):
+        pass
 
-        def initialize_selenium():
-            pass
+    def initialize_selenium():
+        pass
 
-        def login(self):
-            pass
+    def login(self):
+        pass
