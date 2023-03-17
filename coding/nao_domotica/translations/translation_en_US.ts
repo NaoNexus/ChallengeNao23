5ef72b8d40bb2fa5c7f479a9541b1f917e27c36a
edopol24 <122732877+edopol24@users.noreply.github.com>
2023-03-17 18:03:53 +0100

    Comandi domotica
    aggiornamento

--- coding/nao_domotica/translations/translation_en_US.ts
+++ coding/nao_domotica/translations/translation_en_US.ts
@@ -1,3 +1,64 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="en_US"/>
+<TS version="2.1" language="en_US">
+    <context>
+        <name>behavior_1/behavior.xar:/Animated Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Eseguo subito</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Eseguo subito</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Animated Say (1)</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Ci penso io</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Ci penso io</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Animated Say (2)</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Adesso faccio</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Adesso faccio</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Animated Say (3)</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Faccio subito</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Faccio subito</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Animated Say (4)</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Hello</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Ci penso io</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Ci penso io</translation>
+        </message>
+    </context>
+</TS>
