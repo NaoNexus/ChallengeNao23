f2e4147adf33362e00a0c2d8be73c538d8390d04
edopol24 <122732877+edopol24@users.noreply.github.com>
2023-03-22 10:15:08 +0100

    Domotics update
    Aggiornamento risposte comandi

--- coding/nao_domotica/translations/translation_en_US.ts
+++ coding/nao_domotica/translations/translation_en_US.ts
@@ -9,10 +9,9 @@
             <translation type="vanished">Hello</translation>
         </message>
         <message>
-            <location filename="behavior_1/behavior.xar" line="0"/>
             <source>Eseguo subito</source>
             <comment>Text</comment>
-            <translation type="unfinished">Eseguo subito</translation>
+            <translation type="obsolete">Eseguo subito</translation>
         </message>
     </context>
     <context>
@@ -23,28 +22,25 @@
             <translation type="vanished">Hello</translation>
         </message>
         <message>
-            <location filename="behavior_1/behavior.xar" line="0"/>
             <source>Ci penso io</source>
             <comment>Text</comment>
-            <translation type="unfinished">Ci penso io</translation>
+            <translation type="obsolete">Ci penso io</translation>
         </message>
     </context>
     <context>
         <name>behavior_1/behavior.xar:/Animated Say (2)</name>
         <message>
-            <location filename="behavior_1/behavior.xar" line="0"/>
             <source>Adesso faccio</source>
             <comment>Text</comment>
-            <translation type="unfinished">Adesso faccio</translation>
+            <translation type="obsolete">Adesso faccio</translation>
         </message>
     </context>
     <context>
         <name>behavior_1/behavior.xar:/Animated Say (3)</name>
         <message>
-            <location filename="behavior_1/behavior.xar" line="0"/>
             <source>Faccio subito</source>
             <comment>Text</comment>
-            <translation type="unfinished">Faccio subito</translation>
+            <translation type="obsolete">Faccio subito</translation>
         </message>
     </context>
     <context>
@@ -55,10 +51,63 @@
             <translation type="obsolete">Hello</translation>
         </message>
         <message>
+            <source>Ci penso io</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Ci penso io</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Tapparelle1</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Eseguo subito</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Eseguo subito</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Tapparelle2</name>
+        <message>
             <location filename="behavior_1/behavior.xar" line="0"/>
             <source>Ci penso io</source>
             <comment>Text</comment>
             <translation type="unfinished">Ci penso io</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Luce</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Adesso faccio</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Adesso faccio</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Lim</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Faccio subito</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Faccio subito</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Riscaldamento</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Ci penso io</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Ci penso io</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Progetto</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Ciao, sono NAO. Il nostro progetto prevede di controllare i parametri ambientali di dove mi trovo, in questo modo potrò controllare la domotica in modo di farvi avere un ambiente di lavoro piacevole. Se ci sarò io non dovrete più distrarvi dai vostri progetti per controllare la luce, la presenza di ossigeno, e l’umidità. Se avrete bisogno di spegnere o accendere la luce, alzare o abbassare le tapparelle, spegnere o accendere la LIM potrete chiedermi di farlo. </source>
+            <comment>Text</comment>
+            <translation type="unfinished">Ciao, sono NAO. Il nostro progetto prevede di controllare i parametri ambientali di dove mi trovo, in questo modo potrò controllare la domotica in modo di farvi avere un ambiente di lavoro piacevole. Se ci sarò io non dovrete più distrarvi dai vostri progetti per controllare la luce, la presenza di ossigeno, e l’umidità. Se avrete bisogno di spegnere o accendere la luce, alzare o abbassare le tapparelle, spegnere o accendere la LIM potrete chiedermi di farlo. </translation>
         </message>
     </context>
 </TS>
