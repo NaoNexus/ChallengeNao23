b38ce74d8bfb5b8c0c8da3a939d275f1688b3ba5
edopol24 <122732877+edopol24@users.noreply.github.com>
2023-03-23 15:28:25 +0100

    Domotics wow
    no vabbè wow

--- coding/nao_domotica/translations/translation_en_US.ts
+++ coding/nao_domotica/translations/translation_en_US.ts
@@ -57,6 +57,42 @@
         </message>
     </context>
     <context>
+        <name>behavior_1/behavior.xar:/Lim</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Faccio subito</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Faccio subito</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Luce</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Adesso faccio</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Adesso faccio</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Progetto</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Ciao, sono NAO. Il nostro progetto prevede di controllare i parametri ambientali di dove mi trovo, in questo modo potrò controllare la domotica in modo di farvi avere un ambiente di lavoro piacevole. Se ci sarò io non dovrete più distrarvi dai vostri progetti per controllare la luce, la presenza di ossigeno, e l’umidità. Se avrete bisogno di spegnere o accendere la luce, alzare o abbassare le tapparelle, spegnere o accendere la LIM potrete chiedermi di farlo. </source>
+            <comment>Text</comment>
+            <translation type="unfinished">Ciao, sono NAO. Il nostro progetto prevede di controllare i parametri ambientali di dove mi trovo, in questo modo potrò controllare la domotica in modo di farvi avere un ambiente di lavoro piacevole. Se ci sarò io non dovrete più distrarvi dai vostri progetti per controllare la luce, la presenza di ossigeno, e l’umidità. Se avrete bisogno di spegnere o accendere la luce, alzare o abbassare le tapparelle, spegnere o accendere la LIM potrete chiedermi di farlo. </translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Riscaldamento</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Ci penso io</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Ci penso io</translation>
+        </message>
+    </context>
+    <context>
         <name>behavior_1/behavior.xar:/Tapparelle1</name>
         <message>
             <location filename="behavior_1/behavior.xar" line="0"/>
@@ -72,42 +108,6 @@
             <source>Ci penso io</source>
             <comment>Text</comment>
             <translation type="unfinished">Ci penso io</translation>
-        </message>
-    </context>
-    <context>
-        <name>behavior_1/behavior.xar:/Luce</name>
-        <message>
-            <location filename="behavior_1/behavior.xar" line="0"/>
-            <source>Adesso faccio</source>
-            <comment>Text</comment>
-            <translation type="unfinished">Adesso faccio</translation>
-        </message>
-    </context>
-    <context>
-        <name>behavior_1/behavior.xar:/Lim</name>
-        <message>
-            <location filename="behavior_1/behavior.xar" line="0"/>
-            <source>Faccio subito</source>
-            <comment>Text</comment>
-            <translation type="unfinished">Faccio subito</translation>
-        </message>
-    </context>
-    <context>
-        <name>behavior_1/behavior.xar:/Riscaldamento</name>
-        <message>
-            <location filename="behavior_1/behavior.xar" line="0"/>
-            <source>Ci penso io</source>
-            <comment>Text</comment>
-            <translation type="unfinished">Ci penso io</translation>
-        </message>
-    </context>
-    <context>
-        <name>behavior_1/behavior.xar:/Progetto</name>
-        <message>
-            <location filename="behavior_1/behavior.xar" line="0"/>
-            <source>Ciao, sono NAO. Il nostro progetto prevede di controllare i parametri ambientali di dove mi trovo, in questo modo potrò controllare la domotica in modo di farvi avere un ambiente di lavoro piacevole. Se ci sarò io non dovrete più distrarvi dai vostri progetti per controllare la luce, la presenza di ossigeno, e l’umidità. Se avrete bisogno di spegnere o accendere la luce, alzare o abbassare le tapparelle, spegnere o accendere la LIM potrete chiedermi di farlo. </source>
-            <comment>Text</comment>
-            <translation type="unfinished">Ciao, sono NAO. Il nostro progetto prevede di controllare i parametri ambientali di dove mi trovo, in questo modo potrò controllare la domotica in modo di farvi avere un ambiente di lavoro piacevole. Se ci sarò io non dovrete più distrarvi dai vostri progetti per controllare la luce, la presenza di ossigeno, e l’umidità. Se avrete bisogno di spegnere o accendere la luce, alzare o abbassare le tapparelle, spegnere o accendere la LIM potrete chiedermi di farlo. </translation>
         </message>
     </context>
 </TS>
