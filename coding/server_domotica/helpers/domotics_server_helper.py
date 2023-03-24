761ea263322f17966a03e1f001863626bab8118c
Riccardo Antonelli <rickyanto05@gmail.com>
2023-03-24 16:29:53 +0100

    Updated domotics server

--- coding/server_domotica/helpers/domotics_server_helper.py
+++ coding/server_domotica/helpers/domotics_server_helper.py
@@ -1,7 +1,15 @@
 import helpers.config_helper as config_helper
-from logging_helper import logger
+from helpers.logging_helper import logger
 
-import socket
+from telnetlib import Telnet
+from enum import Enum
+
+
+class Room(Enum):
+    it = 1
+    robotics = 2
+    quinta_ssa = 3
+
 
 class DomoticsServer:
     ip: str
@@ -11,10 +19,75 @@ class DomoticsServer:
         self.ip = config.domotics_ip
         self.port = config.domotics_port
 
-    def send_command(self, command):
-        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-            s.connect((self.ip, self.port))
-            s.sendall(bytes(command, 'utf-8'))
-            data = s.recv(1024)
-            logger.info(data)
-            return data
+    def get_status(self, command):
+        with Telnet(self.ip, self.port) as tn:
+            tn.write((f'GETLOAD {command}\n').encode())
+            response = tn.read_some().decode()
+            response = response[response.rfind(' ')+1:response.rfind('\n')]
+            logger.info(f'Telnet response: {response}')
+            return response
+
+    def send_light_command(self, command):
+        with Telnet(self.ip, self.port) as tn:
+            tn.write((f'LOAD {command}\n').encode())
+            response = tn.read_some().decode()
+            logger.info(f'Telnet response: {response}')
+            return response
+
+    def send_blind_command(self, command):
+        with Telnet(self.ip, self.port) as tn:
+            tn.write((f'BTN {command}\n').encode())
+            response = tn.read_some().decode()
+            logger.info(f'Telnet response: {response}')
+            return response
+
+    def switch_lights(self, room: Room):
+        if (room == Room.robotics):
+            lights = ['15990', '15991']
+        elif (room == Room.it):
+            lights = ['15992', '15993']
+        elif (room == Room.quinta_ssa):
+            lights = ['15986', '15987']
+
+        command = '100'
+
+        if (float(self.get_status(lights[0])) != 0):
+            command = '0'
+
+        for light in lights:
+            self.send_light_command(f'{light} {command}')
+
+    def switch_lim(self, room: Room):
+        if (room == Room.robotics):
+            lim = '19604'
+        elif (room == Room.it):
+            lim = '19605'
+        elif (room == Room.quinta_ssa):
+            lim = '19602'
+
+        command = '100'
+
+        if (float(self.get_status(lim)) != 0):
+            command = '0'
+
+        self.send_light_command(f'{lim} {command}')
+
+    def open_blinds(self, room: Room):
+        if (room == Room.robotics):
+            blind = '16072'
+        elif (room == Room.it):
+            blind = '16082'
+        elif (room == Room.quinta_ssa):
+            blind = '16052'
+
+        self.send_blind_command(blind)
+
+    def close_blinds(self, room: Room):
+        if (room == Room.robotics):
+            blind = '16076'
+        elif (room == Room.it):
+            blind = '16086'
+        elif (room == Room.quinta_ssa):
+            blind = '16056'
+
+        self.send_blind_command(blind)
