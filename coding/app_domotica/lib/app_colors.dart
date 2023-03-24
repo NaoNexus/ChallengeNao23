09873256a9c31925a44a3f925a2db218321e6ab6
Bernaaaa <francescobernardi04@gmail.com>
2023-03-24 18:42:21 +0100

    Updated app UI

--- coding/app_domotica/lib/app_colors.dart
+++ coding/app_domotica/lib/app_colors.dart
@@ -4,6 +4,11 @@ class AppColors {
   static const red = Color(0xFFD9413D);
   static const teal = Color(0xFF368DB3);
   static const blue = Color(0xFF1A2B50);
+  static const orange = Color(0xFFEC8F2D);
+  static const bgcolor = Color(0xFF0B2D39);
+  static const cardbgcolor = Color(0xFF0F3C4C);
+  static const textcolor = Color(0xFFE9F5F9);
+  static const appbarcolor = Color(0xFF);
 }
 
 extension WithLightness on Color {
