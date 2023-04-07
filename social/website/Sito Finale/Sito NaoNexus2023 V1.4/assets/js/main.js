e0c8ead9359e2d19ddbf147ed7dc69e9c0a57a47
ginoilboss <102758469+ginoilboss@users.noreply.github.com>
2023-04-07 18:39:06 +0200

    website

--- social/website/Sito Finale/Sito NaoNexus2023 V1.4/assets/js/main.js
+++ social/website/Sito Finale/Sito NaoNexus2023 V1.4/assets/js/main.js
@@ -4,6 +4,9 @@
 
 	var	$window = $(window),
 		$body = $('body'),
+		$header = $('#header'),
+		$footer = $('#footer'),
+		$main = $('#main'),
 		settings = {
 
 			// Carousels
@@ -12,6 +15,11 @@
 					fadeIn: true,
 					fadeDelay: 250
 				},
+				// Parallax background effect?
+				parallax: true,
+
+			// Parallax factor (lower = more intense, higher = less intense).
+				parallaxFactor: 20
 
 		};
 
@@ -30,7 +38,63 @@
 				$body.removeClass('is-preload');
 			}, 100);
 		});
+	// Touch?
+		if (browser.mobile) {
 
+			// Turn on touch mode.
+				$body.addClass('is-touch');
+
+			// Height fix (mostly for iOS).
+				window.setTimeout(function() {
+					$window.scrollTop($window.scrollTop() + 1);
+				}, 0);
+
+		}
+		
+// Footer.
+		breakpoints.on('<=medium', function() {
+			$footer.insertAfter($main);
+		});
+
+		breakpoints.on('>medium', function() {
+			$footer.appendTo($header);
+		});
+	
+		// Header.
+
+		// Parallax background.
+
+			// Disable parallax on IE (smooth scrolling is jerky), and on mobile platforms (= better performance).
+				if (browser.name == 'ie'
+				||	browser.mobile)
+					settings.parallax = false;
+
+			if (settings.parallax) {
+
+				breakpoints.on('<=medium', function() {
+
+					$window.off('scroll.strata_parallax');
+					$header.css('background-position', '');
+
+				});
+
+				breakpoints.on('>medium', function() {
+
+					$header.css('background-position', 'left 0px');
+
+					$window.on('scroll.strata_parallax', function() {
+						$header.css('background-position', 'left ' + (-1 * (parseInt($window.scrollTop()) / settings.parallaxFactor)) + 'px');
+					});
+
+				});
+
+				$window.on('load', function() {
+					$window.triggerHandler('scroll');
+				});
+
+			}
+
+	
 	// Dropdowns.
 		$('#nav > ul').dropotron({
 			mode: 'fade',
@@ -209,5 +273,25 @@
 				});
 
 		});
+		// Main Sections: Two.
+
+		// Lightbox gallery.
+			$window.on('load', function() {
+
+				$('#two').poptrox({
+					caption: function($a) { return $a.next('h3').text(); },
+					overlayColor: '#2c2c2c',
+					overlayOpacity: 0.85,
+					popupCloserText: '',
+					popupLoaderText: '',
+					selector: '.work-item a.image',
+					usePopupCaption: true,
+					usePopupDefaultStyling: false,
+					usePopupEasyClose: false,
+					usePopupNav: true,
+					windowMargin: (breakpoints.active('<=small') ? 0 : 50)
+				});
+
+			});
 
 })(jQuery);
